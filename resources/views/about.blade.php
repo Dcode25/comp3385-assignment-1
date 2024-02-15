25f0db10ab2d67fe272cc73bbb750c66109b391d
Kiiingdom <video.boy@hotmail.co.uk>
2024-02-15 18:46:26 -0400

    Finished

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,5 @@
 @section('content')
  <h2>Deondre Mayers</h2>
  <p>Hi guys my name is Deondre Mayers and I'm a big headed annoyance and the stress to make sure Dominic ages faster.</p>
+<img src="{{ url('/images/download.jpg') }}" alt="Orange Galaxy" />
 @endsection 
